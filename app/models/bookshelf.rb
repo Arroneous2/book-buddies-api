cf6b5344c1e7fccb01fcef534a680b8d1256477b
Arroneous2 <thumpinbassbeat@gmail.com>
2024-09-18 11:36:37 -0500

    add validation for bookshelf

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -1,4 +1,10 @@
 class Bookshelf < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :status, inclusion: { in: %w(want_to_read reading read), allow_nil: true, allow_blank: true }, on: :update
+  validates :format_type, inclusion: { in: %w(audio print ebook), allow_nil: true, allow_blank: true }, on: :update
+  validates :book_location, inclusion: { in: %w(own loaned waterloo_library, rod_library, cedar_falls_library), allow_nil: true, allow_blank: true }, on: :update
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true, allow_blank: true, on: :update
+
 end
